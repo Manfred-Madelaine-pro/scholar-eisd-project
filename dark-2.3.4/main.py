b0f8a68efa7a532b7ab47ad78a268641fb20eefa
manfred972 <manfred97208@hotmail.fr>
2018-01-18 21:18:54 +0100

    git ignore

--- dark-2.3.4/main.py
+++ dark-2.3.4/main.py
@@ -2,15 +2,22 @@
 
 import os
 
-cmd = "echo \"\
-le mardi 2 janvier \n\
+
+txt = "le mardi 2 janvier \n\
 le mardi 2 \n\
 le mardi 2 janvier 1993 \n\
 le deux janvier \n\
 en mai 1995 \n\
 le 12 août 1995 \n\
 le 1er janvier \n\
-le premier janvier \n\
-\" | ./dark test.lua"
+le premier janvier \
+\n"
+
+
+txt2 = "echo \"\
+ à Tanger (Maroc)\
+\n"
+
+cmd = "echo \"" + txt + "\" | ./dark test.lua"
 
 os.system(cmd) 
