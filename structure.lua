5765f43e8229540f36d0c2f4a2132cb8a54b80cf
manfred972 <manfred97208@hotmail.fr>
2018-01-19 19:37:49 +0100

    début d'ossature de statut

--- structure.lua
+++ structure.lua
@@ -1,16 +1,25 @@
 local struct = {}
 
 function struct.foo()
-	name = "manfred"
-	lastname = "MFD"
-	birth = "1"
+	name = "Jean-Luc"
+	lastname = "Mélenchon"
+	birth = "19 août 1951"
 	death = nil
-	birthplace = "mada"
+	birthplace = "Tanger (Maroc)"
 
-	famille = "yo"
-	formation = "yo"
+	pers = struct.Personne(name, lastname, birth, death, birthplace)
 
-	p = struct.Politician(name, lastname, birth, death, birthplace, famille, formation)
+
+	pere = struct.Personne("Georges", "Mélenchon", nil, nil, "France")
+	mere = struct.Personne("Jeanine", "Bayona", nil, nil, "France")
+
+	famille = {struct.Famille(pere, "père"), struct.Famille(mere, "mère")}
+	
+	formation = nil
+
+	parti = "La France Insoumise"
+
+	p = struct.Politician(pers, famille, formation, parti)
 	struct.print_struct(p)
 end
 
@@ -18,33 +27,13 @@ end
 --[[
 	Constructeur de la classe Politicien
 ]]--
-function struct.Politician(name, lastname, birth, death, birthplace, famille, formation)
-	p = {}
-	p["personne"] = struct.Personne(name, lastname, birth, death, birthplace)
-
-	p["famille"] = famille
-	p["formation"] = formation
-	--[[
-	p[""] = 
-	p[""] = 
-	p[""] = 
-	p[""] = 
-	p[""] = ]]
-	return p
-end
-
-function struct.Politician2(personne, birthplace, famille, formation)
+function struct.Politician(personne, famille, formation, parti)
 	p = {}
 	p["personne"] = personne
 
 	p["famille"] = famille
 	p["formation"] = formation
-	--[[
-	p[""] = 
-	p[""] = 
-	p[""] = 
-	p[""] = 
-	p[""] = ]]
+
 	return p
 end
 
@@ -63,22 +52,27 @@ function struct.Personne(name, lastname, birth, death, birthplace)
 end
 
 
-function struct.Famille()
-	--[[
-		Nom : Georges Mélenchon
-	Profession : receveur des Postes
-	Statut : père
-	Naissance :
-	Décès : 
-	]]
+function struct.Famille(personne, statut)
+	fam = {}
+
+	fam["personne"] = personne
+	fam["statut"] = statut
+
+	return fam
 end
 
 
 function struct.print_struct(my_struct)
 	for index, valeur in pairs(my_struct) do
-    	txt = index.." : "..valeur
+		--print(type(valeur))
+		if type(valeur) == "table" then
+			struct.print_struct(valeur)
+			print("\n")
+		else
+	    	txt = index.." : "..valeur
+    		print(txt)
+		end
 
-    	print(txt)
     end
 end
 
