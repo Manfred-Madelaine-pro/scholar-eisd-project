ded68ae9f35fd5eb57e2a543f6e70d9b44e88185
KissSwag <leo1010@hotmail.fr>
2018-03-04 18:31:53 +0100

    Oui

---
+++ systeme_de_dialogue/couverture.lua
@@ -0,0 +1,30 @@
+Couverture
+Nombre de personnalités politiques : 7467
+name : 7467 couverture : 100.0%
+firstname : 7467 couverture : 100.0%
+birth : 5685 couverture : 76.134993973483%
+birthplace : 5939 couverture : 79.536627829115%
+particule : 7467 couverture : 100.0%
+famille : 813 couverture : 10.887906789875%
+	 Nombre total de membres de famille : 1513
+	 Nom : 642 couverture : 42.432253800397%
+	 Prénom : 1513 couverture : 100.0%
+	 Statut : 1513 couverture : 100.0%
+	 Métier : 193 couverture : 12.756113681428%
+parti : 4363 couverture : 58.430427213071%
+	 Nombre total de partis : 7450
+	 Nom : 7450 couverture : 100.0%
+	 Acronyme : 4659 couverture : 62.536912751678%
+	 Date adhésion : 497 couverture : 6.6711409395973%
+	 Date départ : 497 couverture : 6.6711409395973%
+profession : 5770 couverture : 77.273336011785%
+	 Nombre total de professions : 12760%
+	 Nom : 12760 couverture : 100.0%
+	 Date adhésion : 8263 couverture : 64.757053291536%
+	 Date départ : 6016 couverture : 47.147335423197%
+formation : 263 couverture : 3.5221641890987%
+	 Nombre total de formations : 277
+	 Nom : 277 couverture : 100.0%
+	 Sujet : 179 couverture : 64.620938628159%
+	 Lieu : 18 couverture : 6.4981949458484%
+	 Date obtention : 52 couverture : 18.772563176895%
